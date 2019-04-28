9aeac45811098582850cac17b507effb761f151f
User <user@Users-MacBook-Pro.local>
2019-04-28 09:06:11 -0700

    updats

--- squixel/src/app/components/wrapper.js
+++ squixel/src/app/components/wrapper.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Card from './card';
 
 class Wrapper extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
+  
     render() {
       return (
         <div className='body-container'>
