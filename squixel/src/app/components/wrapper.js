82b14a2dcf5ee72d9a696d262eed4d2a82aa9971
User <user@Users-MacBook-Pro.local>
2019-04-28 09:00:08 -0700

    updates

--- squixel/src/app/components/wrapper.js
+++ squixel/src/app/components/wrapper.js
@@ -4,17 +4,19 @@ import Card from './card';
 class Wrapper extends Component {
     render() {
       return (
-        <div className='wrapperDiv'>
-            {this.props.showCard ? (
-                <Card data={this.props.data}
-                loading={this.props.loading}
-                photos={this.props.photos}
-                showFullScreen={this.props.showFullScreen}
-                showFullScreenImage={this.props.showFullScreenImage}
-                fullScreenImage={this.props.fullScreenImage}
-                />
-            ) : (null)
-            } 
+        <div className='body-container'>
+            <div className='wrapperDiv'>
+                {this.props.showCard ? (
+                    <Card data={this.props.data}
+                    loading={this.props.loading}
+                    photos={this.props.photos}
+                    showFullScreen={this.props.showFullScreen}
+                    showFullScreenImage={this.props.showFullScreenImage}
+                    fullScreenImage={this.props.fullScreenImage}
+                    />
+                ) : (null)
+                } 
+            </div>
         </div>
       );
     }  
