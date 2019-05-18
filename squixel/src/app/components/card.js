88da60deba4b74c1e8db81882d0dde7183635fc9
User <user@Users-MacBook-Pro.local>
2019-05-18 13:14:03 -0700

    adds empty collection message

--- squixel/src/app/components/card.js
+++ squixel/src/app/components/card.js
@@ -8,59 +8,59 @@ class Card extends Component   {
 
   render () {
     if (this.props.loading === false) {
-      return (
-        this.props.data.map((item, id) => {
-          // log all the data
-          // console.log(this.props.data[id]);
-          //For landscape photos
-          if (this.props.data[id].width > this.props.data[id].height){
-            return (
-              <div className='imgSmall' 
-                    key={id}
-                    onClick={() => {
-                      let image = this.props.data[id].urls.full;
-                      let smallImage = this.props.data[id].urls.small;
-                      let width = this.props.data[id].width;
-                      let height = this.props.data[id].height;
-                      let fullscreenData = this.props.data[id];
-                      this.props.showFullScreenImage(image, smallImage, width, height, fullscreenData)
-                    }}
-                >
-                  <img src={this.props.data[id].urls.small} 
-                    key={id} 
-                    width={this.props.data[id].width}
-                    height={this.props.data[id].height}
-                    alt={this.props.data[id].description}
-                    className='landscape'
-                  />
-              </div>
-            )
-          //For Portrait photos
-          } else if (this.props.data[id].width < this.props.data[id].height) {
-            return (
-              <div className='imgSmall' 
-                    key={id}
-                    onClick={() => {
-                      let image = this.props.data[id].urls.full;
-                      let smallImage = this.props.data[id].urls.small;
-                      let width = this.props.data[id].width;
-                      let height = this.props.data[id].height;
-                      let fullscreenDataID = this.props.data[id];
-                      this.props.showFullScreenImage(image, smallImage, width, height, fullscreenDataID)
-                    }}
-                >
-                  <img src={this.props.data[id].urls.small} 
-                    key={id} 
-                    width={this.props.data[id].width}
-                    height={this.props.data[id].height}
-                    alt={this.props.data[id].description}
-                    className='portrait'
-                  />
-              </div>
-            )
-          }
-        })
-      ) 
+        return (
+          this.props.data.map((item, id) => {
+            // log all the data
+            // console.log(this.props.data[id]);
+            //For landscape photos
+            if (this.props.data[id].width > this.props.data[id].height){
+              return (
+                <div className='imgSmall' 
+                      key={id}
+                      onClick={() => {
+                        let image = this.props.data[id].urls.full;
+                        let smallImage = this.props.data[id].urls.small;
+                        let width = this.props.data[id].width;
+                        let height = this.props.data[id].height;
+                        let fullscreenData = this.props.data[id];
+                        this.props.showFullScreenImage(image, smallImage, width, height, fullscreenData)
+                      }}
+                  >
+                    <img src={this.props.data[id].urls.small} 
+                      key={id} 
+                      width={this.props.data[id].width}
+                      height={this.props.data[id].height}
+                      alt={this.props.data[id].description}
+                      className='landscape'
+                    />
+                </div>
+              )
+            //For Portrait photos
+            } else if (this.props.data[id].width < this.props.data[id].height) {
+              return (
+                <div className='imgSmall' 
+                      key={id}
+                      onClick={() => {
+                        let image = this.props.data[id].urls.full;
+                        let smallImage = this.props.data[id].urls.small;
+                        let width = this.props.data[id].width;
+                        let height = this.props.data[id].height;
+                        let fullscreenDataID = this.props.data[id];
+                        this.props.showFullScreenImage(image, smallImage, width, height, fullscreenDataID)
+                      }}
+                  >
+                    <img src={this.props.data[id].urls.small} 
+                      key={id} 
+                      width={this.props.data[id].width}
+                      height={this.props.data[id].height}
+                      alt={this.props.data[id].description}
+                      className='portrait'
+                    />
+                </div>
+              )
+            }
+          })
+        ) 
     }else {
         return ( 
           <div>
--- squixel/src/app/components/userCard.js
+++ squixel/src/app/components/userCard.js
@@ -4,61 +4,64 @@ class UserCard extends Component   {
 
   render () {
     if (this.props.userCardLoading === false) {
-      return (
-        this.props.userCollectionData.map((item, id) => {
-            console.log(this.props.userCollectionData[id]);
-          //For landscape photos
-        //   if (this.props.userCollectionData[id].width > this.props.userCollectionData[id].height){
-            return (
-              <div className='imgSmall' 
-                    key={id}
-                    onClick={() => {
-                      let image = this.props.userCollectionData[id].image;
-                      let width = this.props.userCollectionData[id].width;
-                      let height = this.props.userCollectionData[id].height;
-                      let fullscreenData = this.props.userCollectionData[id];
-                      this.props.showFullScreenImage(image, width, height, fullscreenData)
-                    }}
-                >
-                  <img src={this.props.userCollectionData[id].smallImage} 
-                       key={id} 
-                       width={this.props.userCollectionData[id].width}
-                       height={this.props.userCollectionData[id].height}
-                       alt={"temp description"}
-                       className='landscape'
-                  />
-              </div>
-            )
-          //For Portrait photos
-        //   } 
-        //   else if (this.props.userCollectionData[id].width < this.props.userCollectionData[id].height) {
-        //     return (
-        //       <div className='imgSmall' 
-        //             key={id}
-        //             onClick={() => {
-        //               let image = this.props.userCollectionData[id].urls.full;
-        //               let width = this.props.userCollectionData[id].width;
-        //               let height = this.props.userCollectionData[id].height;
-        //               let fullscreenData = this.props.userCollectionData[id];
-        //               this.props.showFullScreenImage(image, width, height, fullscreenData)
-        //             }}
-        //         >
-        //           <img src={this.props.userCollectionData[id].urls.small} 
-        //             key={id} 
-        //             width={this.props.userCollectionData[id].width}
-        //             height={this.props.userCollectionData[id].height}
-        //             alt={this.props.userCollectionData[id].description}
-        //             className='portrait'
-        //           />
-        //       </div>
-        //     )
-        //   }
-        })
-      ) 
-    }else {
+      if(!this.props.data) {
         return ( 
-          null
+          <div className='emptyCollectionMessage'>Your collection is empty</div>
         )
+      }
+      else {
+        return (
+          this.props.userCollectionData.map((item, id) => {
+              // console.log(this.props.userCollectionData[id]);
+            //For landscape photos
+          //   if (this.props.userCollectionData[id].width > this.props.userCollectionData[id].height){
+              return (
+                <div className='imgSmall' 
+                      key={id}
+                      onClick={() => {
+                        let image = this.props.userCollectionData[id].image;
+                        let width = this.props.userCollectionData[id].width;
+                        let height = this.props.userCollectionData[id].height;
+                        let fullscreenData = this.props.userCollectionData[id];
+                        this.props.showFullScreenImage(image, width, height, fullscreenData)
+                      }}
+                  >
+                    <img src={this.props.userCollectionData[id].smallImage} 
+                        key={id} 
+                        width={this.props.userCollectionData[id].width}
+                        height={this.props.userCollectionData[id].height}
+                        alt={"temp description"}
+                        className='landscape'
+                    />
+                </div>
+              )
+            //For Portrait photos
+          //   } 
+          //   else if (this.props.userCollectionData[id].width < this.props.userCollectionData[id].height) {
+          //     return (
+          //       <div className='imgSmall' 
+          //             key={id}
+          //             onClick={() => {
+          //               let image = this.props.userCollectionData[id].urls.full;
+          //               let width = this.props.userCollectionData[id].width;
+          //               let height = this.props.userCollectionData[id].height;
+          //               let fullscreenData = this.props.userCollectionData[id];
+          //               this.props.showFullScreenImage(image, width, height, fullscreenData)
+          //             }}
+          //         >
+          //           <img src={this.props.userCollectionData[id].urls.small} 
+          //             key={id} 
+          //             width={this.props.userCollectionData[id].width}
+          //             height={this.props.userCollectionData[id].height}
+          //             alt={this.props.userCollectionData[id].description}
+          //             className='portrait'
+          //           />
+          //       </div>
+          //     )
+          //   }
+          })
+        )
+      }
     }  
   }
 }
