2f3115af6fbf97878b847c5697b526c60b09188b
User <user@Users-MacBook-Pro.local>
2019-05-18 14:19:06 -0700

    cleans up some console log comments

--- squixel/server/routes/router.js
+++ squixel/server/routes/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 //changed ./models/user.js  to  ../models/user.js
 const User =  require('../models/user.js');
 
-
+// POST route for user registration
 router.post('/register', function(request, response){
       var u = new User({
         email: request.body.email,
@@ -19,7 +19,7 @@ router.post('/register', function(request, response){
     response.json({msg: response.body});
 });
 
-
+// POST route for user login
 router.post('/login', function(request, response){
   console.log(`Email: ${request.body.email}`);
   console.log(`Password: ${request.body.password}`);
@@ -57,7 +57,7 @@ router.get('/profile', function (request, response, next) {
 });
 
 
-// works mofo 
+// PUT route for adding image to db
 router.put('/add-image', function (request, response) {
   let email = request.body.email;
   let image = request.body.image;
@@ -65,17 +65,15 @@ router.put('/add-image', function (request, response) {
   User.add_image(email, image)
   return response
 })
-// =================================== curent ====================================
 
-// no workie
+// GET route for retrieving all images from user db
 router.get('/getUserContent', function (request, response, email) {  
   User.getUserContent(email)
 })
 
-// =================================== end curent====================================
 
 
-// works mofo, posts console log in mongod terminal
+// GET route to console log in mongod terminal
 router.get('/users', function (request, response) {
   User.findAll();
 })
